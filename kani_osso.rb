c495f5d6cb0228afe712b50bc5b80ec0d5911ba0
peinorou <jupeiiii@gmail.comm>
2019-10-29 11:48:12 +0900

    遅刻時間をtxtファイルに保存するように変更

--- kani_osso.rb
+++ kani_osso.rb
@@ -1,7 +1,12 @@
 require 'discordrb'
 bot = Discordrb::Commands::CommandBot.new token: ENV['DBOT_KANI_OSSO_TOKEN'],
                                       client_id: ENV['DBOT_KANI_OSSO_ID'], prefix: '/'
-late_time_default = 21
+late_time_default = 0
+
+File.open("late_time_default.txt", "r") do |f|
+  late_time_default = f.read.to_i
+end
+
 kani_list = ["かにおっそ", "蟹おっそ"]
 bot.message(containing: kani_list) do |event|
   event.respond "こんにちは!#{event.user.name}さん。遅れて申し訳ございません。\nかにさんは琉球時間で生活しているため、もう少々お待ちください。\n悪気はありません。
@@ -16,6 +21,9 @@ bot.command :late do |event,time|
   late_time = late_time_default + time.to_i
   late_time_default = late_time
   event.respond "かにさんは合計#{late_time}分遅刻しています"
+  File.open("late_time_default.txt", "w+") do |f|
+    f.puts(late_time_default)
+  end
 end
 
 bot.command :kl_help do |event|
